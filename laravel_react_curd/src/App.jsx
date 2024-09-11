208a9b380d635dea76dc4f573e61991a49d04f5e
Md.Sami Ul Haque <samiulh4@gmail.com>
2024-09-11 16:07:10 +0600

    Init Commit

---
+++ laravel_react_curd/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>This Is App js Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
